8d134b8ce4c46daf0da4c048e065834e103fd16c
joshwill234 <234joshwill@gmail.com>
2025-02-26 13:45:00 +0000

    create heroku app update and debug

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cn)c5t*ba0737gke&*vl0y)+4(i7wx&xp2m*vt(n-%fqechi#e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
